fb177b4fe4b7c9f9f808a6e76eb25ce438c61d48
FerdinandWang <603334301@qq.com>
2020-03-19 22:48:41 +0800

    引用类型数组和基本数据类型数组
    创建实例会触发类的初始化
    创建该类型的数组不会触发类的初始化

---
+++ src/com/jvm/classloader/MyTest4.java
@@ -0,0 +1,69 @@
+package com.jvm.classloader;
+
+/**
+ * out1:
+ * MyParent static block
+ *
+ * out2：
+ * class [Lcom.jvm.classloader.MyParent4;
+ *
+ * out3:
+ * class [[Lcom.jvm.classloader.MyParent4;
+ * class java.lang.Object
+ *
+ * out4:
+ * class [I
+ * class java.lang.Object
+ *
+ * 对于数组实例来说，其类型是由JVM在运行期间动态生成的，表示为[Lcom.jvm.classloader.MyParent4
+ * 这样一种形式
+ * 动态生成的类型，其父类型就是Object
+ * 对于数组来说，JavaDoc经常将构成数组的元素为Component，实际上就是将数组降低一个维度后的类型
+ *
+ * 涉及的助记符：
+ * anewarray：表示创建一个引用类型的（如类、接口、数组）数组，并将其引用值压入栈顶
+ * newarray: 表示创建一个指定的原始类型（如int、float、char等）的数组，并将其引用值压入栈顶
+ *
+ */
+public class MyTest4 {
+    public static void main(String[] args) {
+//        1. new 会触发MyParent4的初始化，属于主动使用
+//        MyParent4 myParent4 = new MyParent4();
+
+//        2.
+//        MyParent4[] myParent4s = new MyParent4[1];
+//        System.out.println(myParent4s.getClass());
+
+//        3.
+//        MyParent4[][] myParent4s1 = new MyParent4[1][1];
+//        System.out.println(myParent4s1.getClass());
+//        System.out.println(myParent4s1.getClass().getSuperclass());
+
+//        4.
+//        int[] ints = new int[1];
+//        System.out.println(ints.getClass());
+//        System.out.println(ints.getClass().getSuperclass());
+
+//        5. class [C
+        char [] chars = new char[1];
+        System.out.println(chars.getClass());
+
+//        6. class [Z
+        boolean[] booleans = new boolean[1];
+        System.out.println(booleans.getClass());
+
+//        7. class [S
+        short[] shorts = new short[1];
+        System.out.println(shorts.getClass());
+
+//        8. class [B
+        byte[] bytes = new byte[1];
+        System.out.println(bytes.getClass());
+    }
+}
+
+class MyParent4{
+    static {
+        System.out.println("MyParent static block");
+    }
+}
