a91daeb600016d710e03a38f3d14f4cf8302f8de
FerdinandWang <603334301@qq.com>
2020-03-22 00:12:30 +0800

    自定义类加载器的改造
    深刻理解类的双亲委派机制

--- src/com/jvm/classloader/MyTest16.java
+++ src/com/jvm/classloader/MyTest16.java
@@ -3,7 +3,10 @@ package com.jvm.classloader;
 import java.io.*;
 
 public class MyTest16 extends ClassLoader {
+
     private String classLoaderName;
+
+    private String path;
 
     private final String fileExtension =".class";
 
@@ -17,6 +20,10 @@ public class MyTest16 extends ClassLoader {
         this.classLoaderName = classLoaderName;
     }
 
+    public void setPath(String path) {
+        this.path = path;
+    }
+
     @Override
     public String toString() {
         return "MyTest16{" +
@@ -26,20 +33,25 @@ public class MyTest16 extends ClassLoader {
 
     @Override
     protected Class<?> findClass(String calssName) throws ClassNotFoundException {
+        System.out.println("findClass invoked: " + calssName);
+        System.out.println("class loader name: " + this.classLoaderName);
+
         byte[] data = this.loadClassData(calssName);
         return this.defineClass(calssName, data, 0, data.length);
     }
 
-    private byte[] loadClassData(String name) {
+    private byte[] loadClassData(String className) {
 
         InputStream is = null;
         byte[] data = null;
         ByteArrayOutputStream baos = null;
 
+        className = className.replace(".","/");
+
         try{
             this.classLoaderName = this.classLoaderName.replace(".", "/");
 
-            is = new FileInputStream(new File(name + this.fileExtension));
+            is = new FileInputStream(new File(this.path + className + this.fileExtension));
             baos = new ByteArrayOutputStream();
 
             int ch = 0;
@@ -62,16 +74,34 @@ public class MyTest16 extends ClassLoader {
         return data;
     }
 
-    public static void test(ClassLoader classLoader) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        Class<?> clazz = classLoader.loadClass("com.jvm.classloader.MyTest1");
+
+
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        MyTest16 loader1 = new MyTest16("loader1");
+
+//        loader1.setPath("H://IDEA_workspace/JVMlearning/out/production/JVMlearning/");
+//C://Users/Ferdinand Wang/Desktop
+        loader1.setPath("C://Users/Ferdinand Wang/Desktop/");
+        Class<?> clazz = loader1.loadClass("com.jvm.classloader.MyTest1");
+
+        System.out.println("class: " + clazz.hashCode());
 
         Object object = clazz.newInstance();
 
         System.out.println(object);
+
+        System.out.println("------------------------------");
+
+        MyTest16 loader2 = new MyTest16("loader2");
+        loader2.setPath("C://Users/Ferdinand Wang/Desktop/");
+        Class<?> clazz2 = loader2.loadClass("com.jvm.classloader.MyTest1");
+
+        System.out.println("class: " + clazz2.hashCode());
+        Object object2 = clazz2.newInstance();
+        System.out.println(object2);
+
+        System.out.println();
     }
 
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
-        MyTest16 loader1 = new MyTest16("loader1");
-        test(loader1);
-    }
+
 }
