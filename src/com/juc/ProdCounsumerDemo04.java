f34ed057e05e0c4793be13dd504758991d794c04
FerdinandWang <603334301@qq.com>
2020-05-27 18:04:13 +0800

    可重入锁的使用

--- src/com/juc/ProdCounsumerDemo04.java
+++ src/com/juc/ProdCounsumerDemo04.java
@@ -1,5 +1,8 @@
 package com.juc;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 题目:现在两个线程,可以操作初始值为0的一个变量,
  * 实现一个线程对该变量加1,一个县城对该变量减1.
@@ -9,6 +12,10 @@ package com.juc;
  * 2.判断+干活+通知
  * 3.防止线程的虚拟唤醒,只要有wait需要用while判断
  *
+ * wait()判断的时候用while 不要用 if(官方文档)
+ *
+ *
+ *
  */
 public class ProdCounsumerDemo04 {
 
@@ -17,7 +24,7 @@ public class ProdCounsumerDemo04 {
         Aircondition aircondition = new Aircondition();
 
         new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 100; i++) {
                 try {
                     aircondition.increment();
                 } catch (Exception e) {
@@ -26,25 +33,45 @@ public class ProdCounsumerDemo04 {
             }
         }, "A").start();
 
-        new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+/*        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
                 try {
                     aircondition.decrement();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-        }, "B").start();
+        }, "B").start();*/
 
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    aircondition.increment();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "C").start();
+
+/*        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    aircondition.decrement();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "D").start();*/
     }
 }
 
 class Aircondition {
+
     private int number = 0;
 
-    public synchronized void increment() throws Exception {
+/*    public synchronized void increment() throws Exception {
         //1.判断
-        if(number != 0) {
+        while (number != 0) {
             this.wait();
         }
 
@@ -58,7 +85,7 @@ class Aircondition {
 
     public synchronized void decrement() throws Exception {
         //1.判断
-        if(number == 0) {
+        while (number == 0) {
             this.wait();
         }
 
@@ -67,6 +94,35 @@ class Aircondition {
         System.out.println(Thread.currentThread().getName() + "\t" +  number);
 
         this.notifyAll();
+    }*/
+
+    //可重入非公平的递归锁
+    private final Lock lock = new ReentrantLock();
+
+    public void increment() throws Exception {
+        lock.lock();
+        try{
+            //1.判断
+            while (number != 0) {
+                this.wait();
+            }
+
+            //2.干活
+            number++;
+            System.out.println(Thread.currentThread().getName() + "\t" +  number);
+            //3.通知
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+
+
+
+
     }
+
+
 
 }
