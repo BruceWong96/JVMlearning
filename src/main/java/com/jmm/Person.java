6f098006b6d10b22a628d358079c5346909ac37b
FerdinandWang <603334301@qq.com>
2020-06-02 23:42:03 +0800

    修改目录结构

---
+++ src/main/java/com/jmm/Person.java
@@ -0,0 +1,17 @@
+package com.jmm;
+
+public class Person {
+    public Person(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    private String personName;
+}
